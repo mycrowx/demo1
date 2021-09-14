e2ca384456d7570a291dcda142a657a13c8887c0
Khoa Doan <71146300+KhoaDoanZXC@users.noreply.github.com>
2021-09-14 09:24:12 +0700

    Delete category directory

--- category/src/main/java/com/demo/category/repository/ICategoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.category.repository;
-
-import com.demo.category.model.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
-}
