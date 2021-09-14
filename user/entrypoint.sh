c3b1d68abd69daa1bb66f5cf75ead61aa6a76027
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-09-14 12:42:31 +0700

    ✨ Connect Configuration Server and User

--- user/entrypoint.sh
+++ user/entrypoint.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-java -jar user.jar
+while :
+do
+    status_code_config=$(curl --write-out %{http_code} --silent --output /dev/null http://demo1-config:8011/user/dev)
+
+    echo "Config service response: $status_code_config"
+
+    if [ $status_code_config -eq 200 ]
+    then
+
+        set -eu
+
+        echo "Checking DB connection ..."
+
+        i=0
+        until [ $i -ge 10 ]
+        do
+            nc -z demo1-postgres 5432 && break
+
+            i=$(( i + 1 ))
+
+            echo "$i: Waiting for DB 1 second ..."
+            sleep 1
+        done
+
+        if [ $i -eq 10 ]
+        then
+            echo "DB connection refused, terminating ..."
+            exit 1
+        fi
+
+        echo "DB is up ..."
+        java -jar user.jar
+        break
+    fi
+
+    sleep 3
+done
